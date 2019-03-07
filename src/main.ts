de9777fc11b2531ba0925527e4de7001e447bd5b
Sakthivel <sakthivelsekar33@gmail.com>
2019-03-07 12:07:12 +0530

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "./plugin_config/firebase.ts";
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/plugin_config/firebase.ts
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyAZ4DwoPi59RW_9ALoP9QgCKrXwHoI6r3Q",
+  authDomain: "vue-base-project.firebaseapp.com",
+  databaseURL: "https://vue-base-project.firebaseio.com",
+  projectId: "vue-base-project",
+  storageBucket: "vue-base-project.appspot.com"
+};
+
+firebase.initializeApp(config);
